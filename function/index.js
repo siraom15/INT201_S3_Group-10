2b099e529309326ddd9c20e78eb7e27a0524601d
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-10-18 15:53:33 +0700

    Add more testcase

--- function/index.js
+++ function/index.js
@@ -1,14 +1,16 @@
 import { random } from './myFunction.js';
 
-let randomInArray1 = random(['a','b','c','d','e']);
-console.log(randomInArray1(0));
-console.log(randomInArray1(1));
-console.log(randomInArray1(4));
+let randomInArray1 = random(['a', 'b', 'c', 'd', 'e']);
+console.log("randomInArray1(0) : " , randomInArray1(0));
+console.log("randomInArray1(1) : " , randomInArray1(1));
+console.log("randomInArray1(4) : " , randomInArray1(4));
 
-let randomInArray2 = random([true,false]);
-console.log(randomInArray2());
-console.log(randomInArray2(3));
-console.log(randomInArray2(10));
+console.log("--------");
+let randomInArray2 = random([true, false]);
+console.log("randomInArray2() : " , randomInArray2());
+console.log("randomInArray2(3) : " , randomInArray2(3));
+console.log("randomInArray2(10) : " , randomInArray2(10));
+console.log("--------");
 
 let randomInArray3 = random();
-console.log(randomInArray3());
+console.log("randomInArray3() : " , randomInArray3());
