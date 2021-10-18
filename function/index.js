8d274a77ce350fc5f628c4706fdb30cc3c87e4fd
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-10-18 15:43:05 +0700

    Add more testcase

--- function/index.js
+++ function/index.js
@@ -1,9 +1,14 @@
 import { random } from './myFunction.js';
 
 let randomInArray1 = random(['a','b','c','d','e']);
-console.log(randomInArray1());
+console.log(randomInArray1(0));
 console.log(randomInArray1(1));
 console.log(randomInArray1(4));
 
-let randomInArray2 = random();
+let randomInArray2 = random([true,false]);
 console.log(randomInArray2());
+console.log(randomInArray2(3));
+console.log(randomInArray2(10));
+
+let randomInArray3 = random();
+console.log(randomInArray3());
