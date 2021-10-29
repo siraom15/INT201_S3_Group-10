0906248b1888132ecfdc95ed829a78920a33bf22
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-10-29 17:54:00 +0700

    refactor script.js

--- sushiProject/script.js
+++ sushiProject/script.js
@@ -3,23 +3,26 @@ import { products as prodData } from "./product.js";
 function createImgWithUrl(url) {
     let img = document.createElement("img");
     img.setAttribute("src", url)
-    img.setAttribute("height","150px")
+    img.setAttribute("height", "150px")
     return img
 }
 
-function createElWithClassInnerStyle(elName, className, inner = null, style = "") {
+function createElWithClassInnerStyle(elName, className = undefined, inner = undefined, style = undefined) {
     let el = document.createElement(elName);
-    el.setAttribute("class", className);
-    el.setAttribute("style",style)
-    el.innerHTML = inner
+    className ? el.setAttribute("class", className) : null;
+    style ? el.setAttribute("style", style) : null;
+    inner ? el.innerHTML = inner : null;
     return el;
+}
+function appendManyChilds(parent,childs=[]){
+    childs.forEach(e=>parent.appendChild(e));
 }
 
 let productDiv = document.getElementById("products");
 
 prodData.forEach(e => {
     let col = createElWithClassInnerStyle("div", "col-xs-12 col-md-6 col-lg-4 col-xl-4");
-    let card = createElWithClassInnerStyle("div", "card shadow-sm text-center","","width: 18rem;");
+    let card = createElWithClassInnerStyle("div", "card shadow-sm text-center", "", "width: 18rem;");
     let img = createImgWithUrl(`./assets/images/${e.pictureName}`);
     let cardBody = createElWithClassInnerStyle("div", "card-body");
     let sushiName = createElWithClassInnerStyle("h5", "card-title", e.name);
@@ -29,16 +32,13 @@ prodData.forEach(e => {
     let divAddToCart = createElWithClassInnerStyle("div", "d-flex justify-content-center");
     let btnAddToCart = createElWithClassInnerStyle("a", "btn btn-dark rounded-0 text-center", "เพิ่มลงตะกร้าสินค้า")
 
-    divAddToCart.appendChild(btnAddToCart)
+    divAddToCart.appendChild(btnAddToCart);
 
-    cardBody.appendChild(sushiName);
-    cardBody.appendChild(describe);
-    cardBody.appendChild(price);
-    cardBody.appendChild(remain);
-    cardBody.appendChild(divAddToCart)
-
-    card.appendChild(img);
-    card.appendChild(cardBody);
+    let cardBodyChilds = [sushiName, describe, price, remain, divAddToCart];
+    appendManyChilds(cardBody,cardBodyChilds);
+   
+    let cardChilds = [img, cardBody];
+    appendManyChilds(card,cardChilds);
 
     col.appendChild(card);
     productDiv.appendChild(col);
