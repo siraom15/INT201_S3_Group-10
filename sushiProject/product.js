1d82fed514d34b7292149c5b61ac81225e94613b
KetchupBruh <69072036+KetchupBruh@users.noreply.github.com>
2021-10-29 16:22:40 +0700

    Update product.js

--- sushiProject/product.js
+++ sushiProject/product.js
@@ -1,10 +1,35 @@
-let products = [{
-    id: "Id Here",
-    name: "Name Here",
-    describe: "Desc Here",
-    pictureName: "Url here",
-    price: 0,
-    remainInStock: 0,
-}];
-
+let products = [
+    {
+        id : "s001",
+        name : "Kanimiso",
+        describe : "ใน 1 เช็ดจะมี 1 เถาถ่าน",
+        pictureName : "kanimiso.jpeg",
+        price : 000,
+        remainInStock : 100,
+    },
+    {
+        id : "s002",
+        name : "Sashimituna",
+        describe : "ใน 1 เชตจะมีปลาทูน่า 9 ชิ้นพร้อมผัก",
+        pictureName : "sashimituna.jpeg",
+        price : 70,
+        remainInStock : 100,
+    },
+    {
+        id : "s003",
+        name : "Sushituna",
+        describe : "ใน1เชตจะมีชูชิ 1 ชิ้น",
+        pictureName : "sushituna.png",
+        price : 60,
+        remainInStock : 100,
+    },
+    {
+        id : "s004",
+        name : "Unagi",
+        describe : "ใน 1 เชตจะมีชูชิ 1 ชิ้น",
+        pictureName : "unagi.jpeg",
+        price : 80,
+        remainInStock : 100,
+    }    
+]
 export { products };
