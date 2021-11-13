df5c1ffdc3c823746232610c76988d189353ed5b
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-11-14 03:41:45 +0700

    Add css and edit theme.js

--- sushiProject/assets/javascript/theme.js
+++ sushiProject/assets/javascript/theme.js
@@ -1,13 +1,24 @@
-function toggleThemeMode() {
-    let body = document.getElementsByTagName("body")[0];
-    
-    if (body.setAttribute('class','bg-light')) {
-        body.setAttribute('class','bg-dark')
-      
-    } else if(body.setAttribute('class','bg-dark')) {
-        body.setAttribute('class','bg-light')
+let Theme = {
+  toggle: function () {
+    // toggle
+    let body = document.getElementsByTagName('body')[0];
+    if (body.setAttribute('class', 'bg-light')) {
+      body.setAttribute('class', 'bg-black');
+    } else if (body.setAttribute('class', 'bg-black')) {
+      body.setAttribute('class', 'bg-light');
     }
-}
+  },
+  load: function () {
+    // load setting from cookie
+  },
+  save: function () {
+    // save setting to cookie
+  },
+};
 
 let themeBtn = document.getElementById('themeBtn');
-themeBtn.addEventListener('click',toggleThemeMode);
+themeBtn.addEventListener('click', (e) => {
+  Theme.toggle();
+});
+
+export default Theme;
