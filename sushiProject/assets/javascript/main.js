da0f6502ca4452ad0e20347aef2b945c5eec28e8
KetchupBruh <69072036+KetchupBruh@users.noreply.github.com>
2021-11-13 20:00:15 +0700

    Update main.js

--- sushiProject/assets/javascript/main.js
+++ sushiProject/assets/javascript/main.js
@@ -2,6 +2,7 @@ import Product from './product.js';
 import './product.js';
 import './search.js';
 import './cart.js';
+import './theme.js';
 
 document.addEventListener('DOMContentLoaded', (e) => {
   Product.render();
