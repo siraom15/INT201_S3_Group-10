b1ebb13b4ccb718da8a34e34db8a7bb5bf77d483
jubbyr <87378794+jubbyr@users.noreply.github.com>
2021-11-14 13:16:35 +0700

    Darkmode onclick

--- sushiProject/assets/javascript/theme.js
+++ sushiProject/assets/javascript/theme.js
@@ -1,25 +1,30 @@
-let Theme = {
-  isDarkMode: false,
-  toggle: function () {
-    // toggle
-    let body = document.getElementsByTagName('body')[0];
-    if (body.setAttribute('class', 'bg-light')) {
-      body.setAttribute('class', 'bg-black');
-    } else if (body.setAttribute('class', 'bg-black')) {
-      body.setAttribute('class', 'bg-light');
-    }
-  },
-  loadCookie: function () {
-    // load setting from cookie
-  },
-  saveCookie: function () {
-    // save setting to cookie
-  },
-};
+const Darkmode = document.getElementById('themeBtn');
 
-let themeBtn = document.getElementById('themeBtn');
-themeBtn.addEventListener('click', (e) => {
-  Theme.toggle();
-});
+Darkmode.addEventListener('click', ()=>{
+  document.body.classList.toggle('dark');
+})
+// let Theme = {
+//   isDarkMode: false,
+//   toggle: function () {
+//     // toggle
+//     let body = document.getElementsByTagName('body')[0];
+//     if (body.setAttribute('class', 'bg-light')) {
+//       body.setAttribute('class', 'bg-black');
+//     } else if (body.setAttribute('class', 'bg-black')) {
+//       body.setAttribute('class', 'bg-light');
+//     }
+//   },
+//   loadCookie: function () {
+//     // load setting from cookie
+//   },
+//   saveCookie: function () {
+//     // save setting to cookie
+//   },
+// };
 
-export default Theme;
+// let themeBtn = document.getElementById('themeBtn');
+// themeBtn.addEventListener('click', (e) => {
+//   Theme.toggle();
+// });
+
+// export default Theme;
