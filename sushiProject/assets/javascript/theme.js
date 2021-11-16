2db881d051130c27530c2c468e29f2f3b1168e1d
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-11-16 15:59:45 +0700

    Add DarkMode Function But in comment

--- sushiProject/assets/javascript/theme.js
+++ sushiProject/assets/javascript/theme.js
@@ -6,28 +6,78 @@ Darkmode.addEventListener('click', ()=>{
   toggleBtn.classList.toggle('btn-light');
   searchBtn.classList.toggle('btn-light');
 })
+// import CookieUtil from './cookieUtil.js';
+
 // let Theme = {
 //   isDarkMode: false,
+//   init: function () {
+//     this.loadCookie();
+//     this.render();
+//   },
 //   toggle: function () {
-//     // toggle
-//     let body = document.getElementsByTagName('body')[0];
-//     if (body.setAttribute('class', 'bg-light')) {
-//       body.setAttribute('class', 'bg-black');
-//     } else if (body.setAttribute('class', 'bg-black')) {
-//       body.setAttribute('class', 'bg-light');
-//     }
+//     this.isDarkMode = !this.isDarkMode;
+//     this.saveCookie();
+//     this.render();
 //   },
 //   loadCookie: function () {
-//     // load setting from cookie
+//     this.isDarkMode = JSON.parse(CookieUtil.get('darkmode'));
 //   },
 //   saveCookie: function () {
-//     // save setting to cookie
+//     CookieUtil.set('darkmode', this.isDarkMode);
+//   },
+//   render: function () {
+//     let searchBtn = document.getElementById('searchBtn');
+//     let toggleBtn = document.getElementById('toggleBtn');
+//     let cartBtn = document.getElementById('cartBtn');
+//     let themeBtn = document.getElementById('themeBtn');
+//     let body = document.body;
+//     let card = document.getElementsByClassName('card');
+//     let navbar = document.getElementById('navbar');
+//     let cartModal = document.getElementById('cartModal');
+
+//     if (this.isDarkMode) {
+//       body.classList.add('bg-dark');
+
+//       navbar.classList.remove('navbar-light', 'bg-light', 'text-dark');
+//       navbar.classList.add('navbar-dark', 'bg-dark', 'text-white');
+
+//       toggleBtn.classList.remove('btn-outline-dark');
+//       toggleBtn.classList.add('btn-outline-light');
+
+//       searchBtn.classList.remove('btn-outline-dark');
+//       searchBtn.classList.add('btn-outline-light');
+
+//       cartBtn.classList.remove('btn-outline-dark');
+//       cartBtn.classList.add('btn-outline-light');
+
+//       themeBtn.classList.remove('btn-outline-dark');
+//       themeBtn.classList.add('btn-outline-light');
+//     } else {
+//       body.classList.remove('bg-dark');
+
+//       navbar.classList.add('navbar-light', 'bg-light', 'text-dark');
+//       navbar.classList.remove('navbar-dark', 'bg-dark', 'text-white');
+
+//       toggleBtn.classList.add('btn-outline-dark');
+//       toggleBtn.classList.remove('btn-outline-light');
+
+//       searchBtn.classList.add('btn-outline-dark');
+//       searchBtn.classList.remove('btn-outline-light');
+
+//       cartBtn.classList.add('btn-outline-dark');
+//       cartBtn.classList.remove('btn-outline-light');
+
+//       themeBtn.classList.add('btn-outline-dark');
+//       themeBtn.classList.remove('btn-outline-light');
+//     }
 //   },
 // };
 
-// let themeBtn = document.getElementById('themeBtn');
-// themeBtn.addEventListener('click', (e) => {
+// const DarkmodeBtn = document.getElementById('themeBtn');
+
+// DarkmodeBtn.addEventListener('click', (e) => {
 //   Theme.toggle();
 // });
 
 // export default Theme;
+
