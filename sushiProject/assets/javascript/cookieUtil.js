e7b9fa3de346777e42d54a516996a4ddf7d438a2
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-11-21 14:49:48 +0700

    Refactor CookieUtil

--- sushiProject/assets/javascript/cookieUtil.js
+++ sushiProject/assets/javascript/cookieUtil.js
@@ -1,33 +1,25 @@
-class CookieUtil {
-  static get(name) {
-    let cookieName = `${encodeURIComponent(name)}=`,
-      cookieStart = document.cookie.indexOf(cookieName),
-      cookieValue = null;
-    if (cookieStart > -1) {
-      let cookieEnd = document.cookie.indexOf(';', cookieStart);
-      if (cookieEnd == -1) {
-        cookieEnd = document.cookie.length;
+let CookieUtil = {
+  get: function (name) {
+    let cookieStr = document.cookie;
+    let value = null;
+    cookieStr.split(';').forEach((e) => {
+      let [key, val] = e.trim().split('=');
+      if (key === name) {
+        value = val;
       }
-      cookieValue = decodeURIComponent(
-        document.cookie.substring(cookieStart + cookieName.length, cookieEnd)
-      );
-    }
-
-    return cookieValue;
-  }
-
-  static set(name, value, expires) {
-    let cookieText = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
-
+    });
+    return value;
+  },
+  set: function (name, value, expires) {
+    let cookieStr = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
     if (expires instanceof Date) {
-      cookieText += `; expires=${expires.toUTCString()}`;
+      cookieStr += `; expires=${expires.toUTCString()}`;
     }
+    document.cookie = cookieStr;
+  },
+  unset: function (name) {
+    this.set(name, '', new Date(0));
+  },
+};
 
-    document.cookie = cookieText;
-  }
-
-  static unset(name) {
-    CookieUtil.set(name, '', new Date(0));
-  }
-}
 export default CookieUtil;
